45c751ea0a6d49dab94c620fdaed2386bb2efa64
VIVEKANAND <sinhavivekanand8505@gmail.com>
2022-05-07 23:51:14 +0530

    working fine

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                import snake
+            if event.key == pygame.K_BACKSPACE:
+                import snake
+
+
+        
+    screen.fill((0,212,53))
+    pygame.display.update()
